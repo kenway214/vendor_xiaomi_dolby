ed3cda460eda7ed5b5023622da08879938c55d83
someone5678 <nemui3353@gmail.com>
2022-03-03 16:19:05 +0900

    sweet: Import OnePlus Dolby
    [EDIT]
    * Import Audio Effects entries
    * Drop related blobs
    * Add entry to devicesettings
    * Remove AUDIO_OUTPUT_FLAG_RAW and AUDIO_OUTPUT_FLAG_COMPRESS_OFFLOAD from configs
      to fix effects not applied issue
    
    ref:
    https://github.com/Project-Zephyrus/android_device_oneplus_sm8250-common
    https://github.com/Project-Zephyrus/android_vendor_oneplus_sm8250-common
    
    corresponding repo changes:
    * Import required blobs and package only. otherwise EQ not working
    * DaxUI will overrides AudioFX and MusicFX
    * Set dolby database to OnePlus because we're using OnePlus blobs. otherwise this also causes EQ issue.
    * Keep ro.vendor.dolby.dax.version because we're using xiaomi's A12 daxService
    * Add dolby entries to audio_effects.conf
    * Include dolby profile to media_codecs_vendor_audio.xml
    * Remove libswvqe/libswgamedap entries
    
    corresponding repo:
    https://gitlab.com/someone5678/vendor_dolby
    
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: ziasam <ziauddinsameer@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Co-authored-by: MB <mb150996@gmail.com>
    Co-authored-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Change-Id: Iefbf5187d88bbffce8a228851067bb352f79ba5e
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,7 @@
 DEVICE_PATH := device/xiaomi/sweet
 
 # Audio
+AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 # inherit from the proprietary version
 -include vendor/xiaomi/sweet/BoardConfigVendor.mk
