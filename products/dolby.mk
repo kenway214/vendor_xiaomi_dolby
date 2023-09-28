b9a46668a66f6647ee9abe8c362efed10cceb614
someone5678 <someone5678@users.noreply.github.com>
2023-09-28 22:22:41 +0900

    dolby: Update path
    Change-Id: I2d1d7b93fb2eb97518c83c7f85501273a35b3527

--- products/dolby.mk
+++ products/dolby.mk
@@ -1,11 +1,13 @@
+DOLBY_PATH := vendor/xiaomi/dolby
+
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/dolby/products
+    $(DOLBY_PATH)/products
 
 PRODUCT_PACKAGES += \
     init.dolby.rc
 
-DEVICE_MANIFEST_FILE += vendor/dolby/products/vintf/dax_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/dolby/products/vintf/dax_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DOLBY_PATH)/products/vintf/dax_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/products/vintf/dax_framework_compatibility_matrix.xml
 
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.product.device.db=OP_DEVICE \
@@ -24,6 +26,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.audio.delta.refresh=true \
     ro.vendor.audio.dolby.surround.enable=true
 
-BOARD_VENDOR_SEPOLICY_DIRS += vendor/dolby/products/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/products/sepolicy/vendor
 
-$(call inherit-product, vendor/dolby/common/common-vendor.mk)
+$(call inherit-product, $(DOLBY_PATH)/common/common-vendor.mk)
